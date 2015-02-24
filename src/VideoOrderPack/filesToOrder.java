f647c087a9f573a45d7ee0eb41d58fa7dcc2c1fc
BenjaTwose <benja.twose.tech@gmail.com>
2015-02-24 09:03:40 +0100

    bug: excepcion no video.k file

--- src/VideoOrderPack/filesToOrder.java
+++ src/VideoOrderPack/filesToOrder.java
@@ -18,7 +18,7 @@ public class filesToOrder {
 
 	
 	public static String baseDirw ;
-	public static int cntmove;
+	public static int cntmove, cntfiles;
 	public static SwingControlDemo swingControlDemo = new SwingControlDemo();
 	
 	
@@ -80,6 +80,8 @@ public class filesToOrder {
         
         if (listFile != null) {
             for (int i = 0; i < listFile.length; i++) {
+            	cntfiles++;
+            	swingControlDemo.lbcntvideos.setText("Progress: "+ cntmove+" / " + cntfiles);
                 if (listFile[i].isDirectory()) {
                     walkin(listFile[i]);
                     //System.out.println("CHANGE DIR --- ");
@@ -93,53 +95,63 @@ public class filesToOrder {
                     
                     
                     if ( listFile[i].getPath().indexOf("FTPVideos")==3){
+                    	
                     	// System.out.println("walking id: " + i);
                     	// Extract the Aca id value
                     	int idini = listFile[i].getPath().indexOf("\\",9);
                     	int idfin = listFile[i].getPath().indexOf("\\",13);
+                    	try {
+	                    	pathsACA[i]=paths[i].substring(idini+1,idfin);
+	                    	//System.out.println(" aca ->  " + pathsACA[i] );
+	                    	
+	                    	// Extract the VId id value  1-6165-1.kids79  189-6292.kids
+	                       	idini = pathsName[i].indexOf("-",1);
+	                       	if ((idfin = pathsName[i].indexOf("-",idini+1))==-1) 
+	                       	{
+	                       		idfin = pathsName[i].indexOf(".",idini+1);
+	                        	
+	                       	}
+	                    	
+	                    	
+	                       	pathsNumV[i]=pathsName[i].substring(idini+1,idfin);
+	                    	//System.out.println(pathsName[i]+" videop id ->  " + pathsNumV[i] );
+	 
+	 
+	                        newPathVideo[i] = baseDirw + "\\" + pathsACA[i]+ "\\" + pathsNumV[i]+ "\\" +pathsName[i]; 
+	                        //kk if (newPathVideo[i] != paths[i])
+	                        //kk {
+	                        	try {
+	                        		pathDest = Paths.get(newPathVideo[i]);
+	                        		if (pathDest.toString().compareTo(listFile[i].toPath().toString()) != 0 ){
+										//Files.move(listFile[i].toPath(),pathDest,StandardCopyOption.REPLACE_EXISTING);
+		                        		System.out.println("entra if -> \n"+ listFile[i].toPath()+"\n"+pathDest);
+										cntmove++;
+										
+										//System.out.println(cntmove);
+										
+	                        		}
+	                        		}
+									
+								 catch (Exception e) {
+									// TODO Auto-generated catch block
+									System.out.printf("### ERROR When move %s\t->\t%s\n",paths[i],newPathVideo[i]);
+									swingControlDemo.commentTextArea.append("ERROR:" +paths[i] +"\n");
+									// e.printStackTrace();
+									
+								 }
+	                        }
                     	
-                    	pathsACA[i]=paths[i].substring(idini+1,idfin);
-                    	//System.out.println(" aca ->  " + pathsACA[i] );
-                    	
-                    	// Extract the VId id value  1-6165-1.kids79  189-6292.kids
-                       	idini = pathsName[i].indexOf("-",1);
-                       	if ((idfin = pathsName[i].indexOf("-",idini+1))==-1) 
-                       	{
-                       		idfin = pathsName[i].indexOf(".",idini+1);
-                        	
-                       	}
-                    	
-                       	pathsNumV[i]=pathsName[i].substring(idini+1,idfin);
-                    	//System.out.println(pathsName[i]+" videop id ->  " + pathsNumV[i] );
- 
- 
-                        newPathVideo[i] = baseDirw + "\\" + pathsACA[i]+ "\\" + pathsNumV[i]+ "\\" +pathsName[i]; 
-                        if (newPathVideo[i] != paths[i])
-                        {
-                        	try {
-                        		pathDest = Paths.get(newPathVideo[i]);
-                        		
-								//Files.move(listFile[i].toPath(),pathDest,StandardCopyOption.REPLACE_EXISTING);
-                        		System.out.println("- "+ listFile[i].toPath()+"->"+pathDest);
-								cntmove++;
-								
-								//System.out.println(cntmove);
-								swingControlDemo.lbcntvideos.setText("Progress: "+cntmove);
-							} catch (Exception e) {
-								// TODO Auto-generated catch block
-								System.out.printf("### ERROR When move %s\t->\t%s\n",paths[i],newPathVideo[i]);
-								swingControlDemo.commentTextArea.append("ERROR:" +paths[i] +"\n");
-								// e.printStackTrace();
-								
-							}
-                        }
+                    	catch (Exception e){
+                    		e.printStackTrace();
                     }
                     
                 }
             }
            
-        } 
-    }
+        
+            }
+           }
+	}
 	
 
 	  
